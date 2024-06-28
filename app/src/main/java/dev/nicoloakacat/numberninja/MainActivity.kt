4234aa69ee18609bbe3218efea1827b9af48a9b0
Nicolò Catania <101711650+NicoloAkaCat@users.noreply.github.com>
2024-06-28 23:47:46 +0200

    changed navHostFragment because of warning

--- app/src/main/java/dev/nicoloakacat/numberninja/MainActivity.kt
+++ app/src/main/java/dev/nicoloakacat/numberninja/MainActivity.kt
@@ -5,6 +5,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import dev.nicoloakacat.numberninja.databinding.ActivityMainBinding
 
@@ -17,7 +18,8 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val navView: BottomNavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
         navView.setupWithNavController(navController)
     }
 }
