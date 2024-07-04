4246def3c026ba4e3225974e9d552c9585205569
andreachicco <andreachicco2002.ac@gmail.com>
2024-07-04 22:29:27 +0200

    add firebase auth

--- app/src/main/java/dev/nicoloakacat/numberninja/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/dev/nicoloakacat/numberninja/ui/profile/ProfileViewModel.kt
@@ -3,11 +3,23 @@ package dev.nicoloakacat.numberninja.ui.profile
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseUser
 
 class ProfileViewModel : ViewModel() {
 
+    private val _user = MutableLiveData<FirebaseUser>(null)
+    val user: LiveData<FirebaseUser> = _user
+
+    fun setUser(user: FirebaseUser?) {
+        this._user.value = user
+    }
+
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = ""
     }
     val text: LiveData<String> = _text
+
+    fun setText(text: String) {
+        this._text.value = text
+    }
 }
