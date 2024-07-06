90f188ef99e09f8433bc19bf20d70e3d73a6ec9f
Nicolò Catania <101711650+NicoloAkaCat@users.noreply.github.com>
2024-07-06 10:19:48 +0200

    refactor max score text

--- app/src/main/java/dev/nicoloakacat/numberninja/UserViewModel.kt
+++ app/src/main/java/dev/nicoloakacat/numberninja/UserViewModel.kt
@@ -14,14 +14,12 @@ class UserViewModel: ViewModel() {
     private val _displayName = MutableLiveData<String?>(null)
     private val _uid = MutableLiveData<String?>(null)
     private val _maxScore = MutableLiveData<Int>(0)
-    private val _maxScoreText = MutableLiveData<String>("Max Score: ${_maxScore.value}")
     private val _nationality = MutableLiveData<Int>(0)
 
     val displayName: LiveData<String?> = _displayName
     val uid: LiveData<String?> = _uid
     val isUserLogged: LiveData<Boolean> = _isUserLogged
     val maxScore: LiveData<Int> = _maxScore
-    val maxScoreText: LiveData<String> = _maxScoreText
     val nationality: LiveData<Int> = _nationality
 
     val setUser = {user: FirebaseUser? ->
@@ -32,12 +30,10 @@ class UserViewModel: ViewModel() {
     }
     val setDataFromDB = {data: UserDB ->
         _maxScore.value = data.maxScore!!
-        _maxScoreText.value = "Max Score: ${_maxScore.value}"
         _nationality.value = data.nationality!!
     }
     val setMaxScore = {digit: Int -> if(digit > _maxScore.value!!){
         _maxScore.value = digit
-        _maxScoreText.value = "Max Score: ${_maxScore.value}"
         //TODO update db
     }}
     val updateMaxScoreDB = {maxScore: Int ->
